12ab0d6ac2e5f584970f4fdd85ac7fb03cf049fe
kyrptonaught <anthonybuttillo10@gmail.com>
2023-05-16 01:22:49 -0400

    Add all the inventories I can think of to small inv

--- src/main/java/net/kyrptonaught/lemclienthelper/mixin/SmallInv/invs/BeaconScreenMixin.java
+++ src/main/java/net/kyrptonaught/lemclienthelper/mixin/SmallInv/invs/BeaconScreenMixin.java
@@ -2,16 +2,16 @@ package net.kyrptonaught.lemclienthelper.mixin.SmallInv.invs;
 
 import net.kyrptonaught.lemclienthelper.SmallInv.SmallInvPlayerInv;
 import net.minecraft.client.gui.DrawableHelper;
-import net.minecraft.client.gui.screen.ingame.ForgingScreen;
+import net.minecraft.client.gui.screen.ingame.BeaconScreen;
 import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(ForgingScreen.class)
-public abstract class AnvilScreenMixin implements SmallInvPlayerInv {
+@Mixin(BeaconScreen.class)
+public abstract class BeaconScreenMixin implements SmallInvPlayerInv {
 
-    @Redirect(method = "drawBackground", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/ingame/ForgingScreen;drawTexture(Lnet/minecraft/client/util/math/MatrixStack;IIIIII)V"))
+    @Redirect(method = "drawBackground", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/ingame/BeaconScreen;drawTexture(Lnet/minecraft/client/util/math/MatrixStack;IIIIII)V"))
     public void drawSmallInv(MatrixStack matrices, int x, int y, int u, int v, int width, int height) {
         if (getIsSmall()) {
             DrawableHelper.drawTexture(matrices, x, y, 0, 0, width, height - 83); //shrink orig texture
---
+++ src/main/java/net/kyrptonaught/lemclienthelper/mixin/SmallInv/invs/Generic3x3ScreenMixin.java
@@ -0,0 +1,28 @@
+package net.kyrptonaught.lemclienthelper.mixin.SmallInv.invs;
+
+import net.kyrptonaught.lemclienthelper.SmallInv.SmallInvPlayerInv;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.gui.screen.ingame.Generic3x3ContainerScreen;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(Generic3x3ContainerScreen.class)
+public abstract class Generic3x3ScreenMixin implements SmallInvPlayerInv {
+
+    @Redirect(method = "drawBackground", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/ingame/Generic3x3ContainerScreen;drawTexture(Lnet/minecraft/client/util/math/MatrixStack;IIIIII)V"))
+    public void drawSmallInv(MatrixStack matrices, int x, int y, int u, int v, int width, int height) {
+        if (getIsSmall()) {
+            DrawableHelper.drawTexture(matrices, x, y, 0, 0, width, height - 83); //shrink orig texture
+
+            DrawableHelper.drawTexture(matrices, x, y + (height - 83) - 1, 0, height - 30, width, 29);//draw hotbar
+            DrawableHelper.drawTexture(matrices, x, y + (height - 83) - 1, 0, height - 86, width, 2); //add extra separator
+        } else DrawableHelper.drawTexture(matrices, x, y, u, v, width, height);
+    }
+
+    @Override
+    public boolean isSmallSupported() {
+        return true;
+    }
+}
