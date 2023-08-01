0e3fa76f4d8cc7926cf466f2ee1c963cc05b9fef
kyrptonaught <anthonybuttillo10@gmail.com>
2023-07-31 20:18:04 -0400

    ScoreboardPlayerInfo LCH checks happen in LOGIN phase now

--- src/main/java/net/kyrptonaught/lemclienthelper/ClientData/ClientDataNetworking.java
+++ src/main/java/net/kyrptonaught/lemclienthelper/ClientData/ClientDataNetworking.java
@@ -3,22 +3,31 @@ package net.kyrptonaught.lemclienthelper.ClientData;
 import io.netty.buffer.Unpooled;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.networking.v1.PacketSender;
+import net.fabricmc.fabric.api.client.networking.v1.ClientLoginNetworking;
+import net.fabricmc.loader.api.FabricLoader;
 import net.kyrptonaught.lemclienthelper.ServerConfigs.ServerConfigsMod;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.Identifier;
 
+import java.util.concurrent.CompletableFuture;
+
 public class ClientDataNetworking {
     public static final Identifier HAS_MODS_PACKET = new Identifier("scoreboardplayerinfo", "has_mods_packet");
 
+
     @Environment(EnvType.CLIENT)
-    public static void sendHasLEMPacket(PacketSender sender, boolean hasOptifine, boolean hasController) {
-        PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
-        buf.writeBoolean(true); //LEMClientHelper, Always true
-        buf.writeBoolean(hasOptifine);
-        buf.writeBoolean(hasController);
-        buf.writeInt(ServerConfigsMod.getConfig().guiScale);
-        buf.writeInt(ServerConfigsMod.getConfig().panScale);
-        sender.sendPacket(HAS_MODS_PACKET, buf);
+    public static void sendHasLEMPacket() {
+        ClientLoginNetworking.registerGlobalReceiver(HAS_MODS_PACKET, (client, handler, buf, listenerAdder) -> {
+            FabricLoader loader = FabricLoader.getInstance();
+
+            PacketByteBuf respondeBuf = new PacketByteBuf(Unpooled.buffer());
+            respondeBuf.writeBoolean(true); //LEMClientHelper, Always true
+            respondeBuf.writeBoolean(ClientDataMod.isOptifineLoaded(loader));
+            respondeBuf.writeBoolean(ClientDataMod.isControllerModLoaded(loader));
+            respondeBuf.writeInt(ServerConfigsMod.getConfig().guiScale);
+            respondeBuf.writeInt(ServerConfigsMod.getConfig().panScale);
+
+            return CompletableFuture.completedFuture(respondeBuf);
+        });
     }
 }
