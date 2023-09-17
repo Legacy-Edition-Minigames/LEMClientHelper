Legacy-Edition-Minigames/LEMClientHelper

commit c8a89b9002a8f177dabbdc9786bfeca696209365

Seperate speed from renderer

src/main/java/net/kyrptonaught/lemclienthelper/hud/GlideHudRenderer.java
@@ -57,13 +57,14 @@ public class GlideHudRenderer {
     private static long lastMinutesSpriteChange = 0;
     private static int currentStopwatchSecondsSprite = 0;
     private static int currentStopwatchMinutesSprite = 0;
+    private static double playerSpeed = 0;
 
     public static void onHudRender(DrawContext context, float v) {
         renderSpeedometer(context, v);
         renderStopwatch(context, v);
     }
 
-    public static double getPlayerSpeed() {
+    public static double calculatePlayerSpeed() {
         MinecraftClient client = MinecraftClient.getInstance();
         var player = client.player;
         var pos = player.getPos();
@@ -83,7 +84,11 @@ public class GlideHudRenderer {
            sqrt(a^2 + b^2) will give meters/tick
            sqrt(a^2 + b^2) * 20 will give the value for the speedometer in m/s
         */
-        return Math.sqrt((dy * dy)+(dx * dx + dz * dz)) * 20;
+        playerSpeed = Math.sqrt((dy * dy)+(dx * dx + dz * dz)) * 20;
+    }
+
+    public static double getPlayerSpeed() {
+        return playerSpeed
     }
 
     public static int calculateArrow(double speed) {
@@ -181,20 +186,13 @@ public class GlideHudRenderer {
             long seconds = elapsedTime / 1000000000L;
             long minutes = seconds / 60;
             seconds %= 60;
-            long milliseconds = (elapsedTime % 1000) / 10;
-            
-            String timer = "";
+            long milliseconds = (elapsedTime / 1000000L) % 1000;
 
-            if (minutes < 1) {
-                timer = String.format("%2d.%2d",  seconds, milliseconds);
-            } else {
-                timer = String.format("%d:%02d.%2d", minutes, seconds, milliseconds);
-            }
-            
+            String timer = String.format("%d:%02d.%03d", minutes, seconds, milliseconds);
 
             int textWidth = client.textRenderer.getWidth(timer);
             int textOffset = -4 - textWidth;
-            context.drawText(client.textRenderer, timer, textOffset, -91, 0xffffff, true);
+            context.drawText(client.textRenderer, timer, textOffset, -92, 0xffffff, true);
            
             context.getMatrices().pop();
         }

src/main/java/net/kyrptonaught/lemclienthelper/hud/HudMod.java
@@ -29,6 +29,7 @@ public class HudMod {
     public static void onInitialize() {
         LEMClientHelperMod.configManager.registerFile(MOD_ID, new HudConfig());
         ClientTickEvents.END_CLIENT_TICK.register(GlideHudRenderer::incrementTimer);
+        ClientTickEvents.END_CLIENT_TICK.register(GlideHudRenderer::calculatePlayerSpeed);
         //register hud's here
         HudRenderCallback.EVENT.register(ArmorHudRenderer::onHudRender);
         HudRenderCallback.EVENT.register(GlideHudRenderer::onHudRender);

commit 255adf71e24f35f7a052fd1977296018e2851887

Resolve missing arguments caused by c8a89b9

src/main/java/net/kyrptonaught/lemclienthelper/hud/GlideHudRenderer.java
@@ -64,8 +64,7 @@ public class GlideHudRenderer {
         renderStopwatch(context, v);
     }
 
-    public static double calculatePlayerSpeed() {
-        MinecraftClient client = MinecraftClient.getInstance();
+    public static double calculatePlayerSpeed(MinecraftClient client) {
         var player = client.player;
         var pos = player.getPos();
         double dx = pos.x - player.lastRenderX;