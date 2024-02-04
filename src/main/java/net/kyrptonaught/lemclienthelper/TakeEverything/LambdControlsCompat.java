8a66fd2c7b184ab24e1b265d073195d1cc423de0
S_N00B <49575478+S-N00B-1@users.noreply.github.com>
2024-02-04 17:51:21 +0000

    Minor cleanup, Comment additions

--- src/main/java/net/kyrptonaught/lemclienthelper/TakeEverything/LambdControlsCompat.java
+++ src/main/java/net/kyrptonaught/lemclienthelper/TakeEverything/LambdControlsCompat.java
@@ -15,13 +15,19 @@ import org.lwjgl.glfw.GLFW;
 
 
 public class LambdControlsCompat implements CompatHandler {
+    private static final MinecraftClient clientInstance = MinecraftClient.getInstance();
+    /**
+     * Using a static variable for the client's instance to avoid resource leak in
+     * the take everything bool, though I'm not sure if checking for the
+     * player is totally nessisary.
+     */
 
     public static void register() {
         MidnightControlsCompat.registerCompatHandler(new LambdControlsCompat());
     }
 
     public static boolean takeEverything() {
-        if (MinecraftClient.getInstance().player != null) {
+        if (clientInstance.player != null) {
             TakeEverythingNetworking.sendTakeEverythingPacket();
             return true;
         }
@@ -32,13 +38,19 @@ public class LambdControlsCompat implements CompatHandler {
     @Override
     public void handle(@NotNull MidnightControlsClient mod) {
         new ButtonBinding.Builder(LEMClientHelperMod.MOD_ID + ".takeeverything")
+                .category(ButtonBinding.INVENTORY_CATEGORY)        
                 .buttons(GLFW.GLFW_GAMEPAD_BUTTON_LEFT_THUMB)
+                .action((client,action,value,buttonState)->takeEverything())
                 .onlyInInventory()
-                .category(ButtonBinding.INVENTORY_CATEGORY)
-                .action((client,action,value,buttonState)->takeEverything()).cooldown()
                 .filter(((client, buttonBinding) -> client.currentScreen instanceof GenericContainerScreen || 
                                                     client.currentScreen instanceof Generic3x3ContainerScreen ||
                                                     client.currentScreen instanceof ShulkerBoxScreen ))
-                .register();
+                .register();                        /**
+                                                     * Filter limits the screens where the keybind is active
+                                                     * Currently can be used in: Barrels/Chests, Shulkers, & Hoppers/Droppers
+                                                     * This can be expanded to allow for more inventory screens,
+                                                     * However I don't think that we will often need to send
+                                                     * a take everything packet for a brewing stand/beacon
+                                                     */
     }
 }
