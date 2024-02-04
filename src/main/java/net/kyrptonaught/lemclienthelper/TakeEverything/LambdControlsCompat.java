27b6acf4ce84e492539e4bccbf88b2d38b129de6
S_N00B <49575478+S-N00B-1@users.noreply.github.com>
2024-02-04 08:51:03 +0000

    Update Midnight Controls, Fix Controller Support

--- src/main/java/net/kyrptonaught/lemclienthelper/TakeEverything/LambdControlsCompat.java
+++ src/main/java/net/kyrptonaught/lemclienthelper/TakeEverything/LambdControlsCompat.java
@@ -4,11 +4,14 @@ import eu.midnightdust.midnightcontrols.client.MidnightControlsClient;
 import eu.midnightdust.midnightcontrols.client.compat.CompatHandler;
 import eu.midnightdust.midnightcontrols.client.compat.MidnightControlsCompat;
 import eu.midnightdust.midnightcontrols.client.controller.ButtonBinding;
-import eu.midnightdust.midnightcontrols.client.controller.PressAction;
 import net.kyrptonaught.lemclienthelper.LEMClientHelperMod;
-import net.minecraft.screen.PlayerScreenHandler;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ingame.GenericContainerScreen;
+import net.minecraft.client.gui.screen.ingame.Generic3x3ContainerScreen;
+import net.minecraft.client.gui.screen.ingame.ShulkerBoxScreen;
 import org.jetbrains.annotations.NotNull;
 import org.lwjgl.glfw.GLFW;
+
 
 
 public class LambdControlsCompat implements CompatHandler {
@@ -17,23 +20,25 @@ public class LambdControlsCompat implements CompatHandler {
         MidnightControlsCompat.registerCompatHandler(new LambdControlsCompat());
     }
 
-    private static final PressAction TAKE_EVERYTHING = (client, button, value, action) -> {
-        if (client.currentScreen != null && client.player != null && !(client.player.currentScreenHandler instanceof PlayerScreenHandler)) {
+    public static boolean takeEverything() {
+        if (MinecraftClient.getInstance().player != null) {
             TakeEverythingNetworking.sendTakeEverythingPacket();
             return true;
         }
         return false;
-    };
+    }
+
 
     @Override
     public void handle(@NotNull MidnightControlsClient mod) {
         new ButtonBinding.Builder(LEMClientHelperMod.MOD_ID + ".takeeverything")
                 .buttons(GLFW.GLFW_GAMEPAD_BUTTON_LEFT_THUMB)
                 .onlyInInventory()
-                .action(TAKE_EVERYTHING)
-                .cooldown()
                 .category(ButtonBinding.INVENTORY_CATEGORY)
-                //.linkKeybind(LEBClientHelperMod.takeEverythingKey)
+                .action((client,action,value,buttonState)->takeEverything()).cooldown()
+                .filter(((client, buttonBinding) -> client.currentScreen instanceof GenericContainerScreen || 
+                                                    client.currentScreen instanceof Generic3x3ContainerScreen ||
+                                                    client.currentScreen instanceof ShulkerBoxScreen ))
                 .register();
     }
 }
