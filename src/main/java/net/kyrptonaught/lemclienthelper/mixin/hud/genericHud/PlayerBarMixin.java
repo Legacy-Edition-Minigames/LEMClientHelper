d77d5e1f52e3cece74fcc004ac419136b54b9b66
S-N00B-1 <49575478+S-N00B-1@users.noreply.github.com>
2025-06-13 21:01:29 -0700

    Disable Experience bar for non-l4j users

--- src/main/java/net/kyrptonaught/lemclienthelper/mixin/hud/genericHud/PlayerBarMixin.java
+++ src/main/java/net/kyrptonaught/lemclienthelper/mixin/hud/genericHud/PlayerBarMixin.java
@@ -1,5 +1,6 @@
 package net.kyrptonaught.lemclienthelper.mixin.hud.genericHud;
 
+import net.kyrptonaught.lemclienthelper.hud.genericHud.GenericHudMod;
 import net.kyrptonaught.lemclienthelper.hud.genericHud.PlayerBarRenderer;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.gui.Gui;
@@ -9,12 +10,15 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.lang.reflect.InvocationTargetException;
-
 @Mixin(Gui.class)
 public class PlayerBarMixin {
     @Inject(method = "renderHotbarAndDecorations", at= @At(value = "HEAD"))
     void renderPlayerBar(GuiGraphics guiGraphics, DeltaTracker deltaTracker, CallbackInfo ci){
         PlayerBarRenderer.renderPlayerBar(guiGraphics,deltaTracker);
+    }
+
+    @Inject(method = "renderExperienceBar", at=@At("HEAD"), cancellable = true)
+    void disableExperienceBar(GuiGraphics guiGraphics, int i, CallbackInfo ci) {
+        if (GenericHudMod.SHOULD_RENDER_PLAYERBAR) {ci.cancel();}
     }
 }
